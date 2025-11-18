348abd3b0ed8e3ecb56f9dd112bd4b201bcfeea5
Notes591 <fivestarown@gmail.com>
2025-11-18 09:51:07 +0300

    Delete gsheets_client.py

--- gsheets_client.py
+++
@@ -1,7 +0,0 @@
-import gspread
-
-def open_sheet_from_service_account(sa_json_path, spreadsheet_id, sheet_name):
-    gc = gspread.service_account(filename=sa_json_path)
-    sh = gc.open_by_key(spreadsheet_id)
-    ws = sh.worksheet(sheet_name)
-    return ws
